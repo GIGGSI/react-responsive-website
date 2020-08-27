3cd373fc6a8482a9ccc4ca4da56789241e77ed2a
Stoyan Spasov <spasovstoyan@gmail.com>
2020-08-28 01:07:34 +0300

    home page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import './App.css';
 import Navbar from './components/pages/Navbar';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Home from './components/pages/HomePage/Home';
 
 function App() {
   return (
     <Router>
       <Navbar />
       <Switch>
-        <Route path="/" />
+        <Route path="/" exact component={Home} />
       </Switch>
     </Router>
   );
