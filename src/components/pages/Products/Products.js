019b5758c8850b4605019818bfe9fa2053aa2245
Stoyan Spasov <spasovstoyan@gmail.com>
2020-08-28 02:21:21 +0300

    Services, Products SignUp are ready

--- src/components/pages/Products/Products.js
+++ src/components/pages/Products/Products.js
@@ -3,14 +3,12 @@ import HeroSection from '../HeroSection';
 import { homeObjOne, homeObjTwo, homeObjThree, homeObjFour } from './Data'
 import Pricing from '../Pricing';
 
-export default function Home() {
+export default function Products() {
     return (
         <>
-            <HeroSection {...homeObjOne} />
-            <HeroSection {...homeObjThree} />
-            <HeroSection {...homeObjTwo} />
+
             <Pricing />
-            <HeroSection {...homeObjFour} />
+            <HeroSection {...homeObjOne} />
         </>
     )
 }
